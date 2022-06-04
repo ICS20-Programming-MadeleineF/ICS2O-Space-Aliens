1a542bcc76ad2086d4e0246f644f1b364c01f8d5
Madeleine Forgeron <99274168+MadeleineForgeron@users.noreply.github.com>
2022-06-04 19:24:28 +0000

    center background

--- js/game.js
+++ js/game.js
@@ -1,5 +1,11 @@
 /* global Phaser */
 
+// Copyright (c) 2020 Mr. Coxall all rights reserved
+//
+// Created by: Mr. Coxall
+// modified by: Madeleine Forgeron
+// Created on: May 2022
+// This file is the phaser3 configuration file
 
 // const
 
@@ -8,7 +14,19 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a 
+  physics: {
+    default: 'arcade' ,
+    arcade: {
+      debug: true
+    }
+  },
+// background colour
+  backgroundColor: 0x5f6e7a ,
+// center background
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
