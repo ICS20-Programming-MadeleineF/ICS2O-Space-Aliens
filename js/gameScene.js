fb61a45a87e4a87277e78e13b2e1ed9f54667e2b
Madeleine Forgeron <99274168+MadeleineForgeron@users.noreply.github.com>
2022-06-13 15:49:36 +0000

    changed end text

--- js/gameScene.js
+++ js/gameScene.js
@@ -94,7 +94,7 @@ class GameScene extends Phaser.Scene {
       shipCollide.destroy()
       this.score = 0
       this.background = this.add.image(1920 / 2, 1080 / 2, 'dog')
-      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over... you have killed your dog\n HOW COULD YOU DO THIS YOU MONSTER!!!!!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
       this.gameOverText.setInteractive({ useHandCursor: true})
       this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
     }.bind(this))
